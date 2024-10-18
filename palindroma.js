2c9b5c86971728fda0fe026f8cc17be0d4f4f7ab
Gioi.Pronesti <gioi.pronesti@gmail.com>
2024-10-18 16:12:16 +0200

    firt commit palindrom word

---
+++ palindroma.js
@@ -0,0 +1,15 @@
+console.log('hello console')
+
+/*
+Palidroma
+Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma
+*/ 
+/*
+ Descrizione del problema
+
+ Utente inserisce una parola 
+ creare una funzione che determini se la parola inserita è palindroma. es "osso, anna "
+
+ // const text = isPalindromword(word)
+    return result /boolean    true/false
+ */
